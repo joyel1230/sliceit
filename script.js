614db57172ebe7fd5bb51ac00cb46279b01c506b
Joyel Joshy <joyeljoshy33@gmail.com>
2022-12-06 17:18:40 +0530

    nav-color

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ var count = 1;
         var property = document.getElementById("bgc");
         if (count == 0) {
             property.style.backgroundColor = "#FFFFFF"
+            document.getElementById("bgc").style.transition = "0.5s";
             count = 1;        
         }
         else {
             property.style.backgroundColor = "#890cbd"
             count = 0;
+            var navlinks = document.getElementsByClassName("afs");
+            console.log(navlinks);
+            for(i=0;i<navlinks.length;i++){
+                navlinks[i].style.color="white"
+            }
+            var btn=document.getElementsByClassName("navbar-toggler")[0];
+            btn.style.color="white"
+
+            // navlinks.forEach((link)=>{
+            //     link.style.color ="white";
+            // })
         }
     }
